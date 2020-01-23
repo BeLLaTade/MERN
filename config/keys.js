cdd52482de80c09dda5e6ddc425f42b034504c0d
BeLLaTade <58252161+BeLLaTade@users.noreply.github.com>
2020-01-23 15:58:45 +0300

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://abel:abel1234@cluster0-ihqu1.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//bodyparser Middleware 
+app.use(bodyparser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected.... '))
+    .catch(err => console.log(err));
+
+// use routes 
+app.use('/api/items', items);
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log('server started on port ${port}'));
